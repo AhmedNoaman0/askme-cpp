030bb68a307ee8463d0424df6aa227fb1ff03b48
AhmedNoman0 <ahmednadi9292@gmail.com>
2026-08-09 13:00:35 +0300

    file system fucnctions for user_controller

--- AskMe_V1.cpp
+++ AskMe_V1.cpp
@@ -1,16 +1,107 @@
 #include<iostream>
 #include<vector>
 #include<map>
+#include<sstream>
+#include<fstream>
+#include<cassert>
+
 using namespace std;
+
+// ^ --------------------------- Helper Functions ---------------------------
+vector<string> ReadFileLines(string& path)
+{
+    vector<string> lines;
+    fstream fin(path);
+    string line;
+    if(fin.fail())
+    {
+        cout<<"Cant open the file :( \n\n";
+        return lines;
+    }
+    while (getline(fin,line))
+    {
+        if(line.size() == 0) // if the line is empty -> ignore this line :)
+            continue;
+        lines.push_back(line);
+    }
+    fin.close();
+    return lines;
+}
+// vector<string> SplitString(const string& line)
+// {
+//     vector<string> fields;
+//     istringstream iss(line);
+//     string field;
+
+//     while (getline(iss,field,','))
+//         fields.push_back(field);
+
+//     if (fields.size() == 6) // leave the answer field as "" if it's empty.
+//         fields.push_back("");
+
+//     return fields;
+// }
+// 1,2,3,4,why you say,ok; ,why,""
+vector<string>SplitString(string& line,string delimiter = ",")
+{
+    vector<string> fields;
+    string field;
+    int pos = 0;
+    while (pos != -1) // if not exist find() -> returns -1
+    {
+        pos = line.find(delimiter);
+        field = line.substr(0,pos); // we remove the previous field, so next one we start from index 0
+        fields.push_back(field);
+        line.erase(0,pos + delimiter.length());
+    }
+    fields.push_back(line); // after finshing, we need to add last field "ok"
+    return fields;
+}
+
+void WriteFileLines(string& path,vector<string>& lines) // to do a bool append or overwrite
+{
+    ofstream fout(path);
+    if(fout.fail()){
+        cout << "\n\nERROR: Can't open the file\n\n";
+        return;}
+
+    for(auto& line : lines)
+        fout<<line<<"\n";
+    fout.close();
+}
 
 struct User
 {
     int id;
     string username;
+    string password;
     string name;
     string email;
-    int password;
-    bool allow_anonymous_qs;
+    int allow_anonymous_qs;
+    User()
+    {
+
+    }
+    User(string& line)
+    {
+        vector<string> fields = SplitString(line);
+        assert(fields.size() == 6);
+
+        id = stoi(fields[0]);
+        username = fields[1];
+        password = fields[2];
+        name = fields[3];
+        email = fields[4];
+        allow_anonymous_qs = stoi(fields[5]);
+
+    }
+    string Toline()
+    {
+        ostringstream oss;
+        oss << id <<","<< username << ","<< password << ","<< name << ","<< email << "," << allow_anonymous_qs;
+        return oss.str();
+    }
+
 };
 
 struct Question
@@ -21,22 +112,59 @@ struct Question
     int to_user_id;
     int is_anonymous_qs;
     string text;
+    string answer;
     Question()
     {
 
     }
-    Question(int from_id,int to_id,string _text)
+    Question(string& line)
     {
-        from_user_id = from_id;
-        to_user_id = to_id;
-        text = _text;
+        vector<string> fields = SplitString(line);
+        assert(fields.size() == 7);
+
+        question_id = stoi(fields[0]);
+        parent_question_id = stoi(fields[1]);
+        from_user_id = stoi(fields[2]);
+        to_user_id = stoi(fields[3]);
+        is_anonymous_qs = stoi(fields[4]);
+        text = fields[5];
+        answer = fields[6];
+
     }
+    void Print_Question_To() // qs to a specific user (not me)
+    {
+        string prefix = ""; // to write thread or question
+
+        if(parent_question_id != -1)
+            prefix = "\tthread";
+
+        cout<<prefix<<"Question Id ("<<question_id<<")";
+
+        if(!is_anonymous_qs)
+            cout<<"from user id("<<from_user_id<<")";
+
+        cout<<"\tQuestion: "<<text;
+
+        if(answer != "")
+            cout<<prefix<<"\tAnswer: "<<answer;
+
+        cout<<"\n";
+    }
+
+    string To_Line()
+    {
+        ostringstream oss;
+        oss<<question_id<<","<<parent_question_id<<","<<from_user_id<<","<<to_user_id<<","
+        <<is_anonymous_qs<<","<<text<<","<<answer;
+        return oss.str();
+    }
+
 };
 
 struct User_Controller
 {
     User current_user;
-    int last_id = 0;
+    int last_id;
     map<string,User> username_object_map;
 
     pair<int,int> ReadUserId()
@@ -55,7 +183,19 @@ struct User_Controller
         return ReadUserId();
     }
 
-
+    void Load_Users()
+    {
+        username_object_map.clear();
+        last_id = 0;
+        string path = "data/users.txt";
+        vector<string> lines = ReadFileLines(path);
+        for(auto& line : lines)
+        {
+            User user(line);
+            last_id = max(last_id,user.id);
+            username_object_map[user.name] = user;
+        }
+    }
     void Login()
     {
         while (true)
@@ -111,10 +251,73 @@ struct Question_Controller
     map<int,Question> QsId_object_map; // to access object by his id
 
     int last_id; // auto incrment 
+    
     Question_Controller()
     {
         last_id = 0;
     }
+    void Load_Quesitons()
+    {
+        QsId_object_map.clear();
+        parentQsId_childQsId_map.clear();
+        last_id = 0;
+
+        string path = "data/questions.txt";
+        vector<string> lines = ReadFileLines(path);
+        for(auto& line : lines)
+        {
+            Question question(line);
+            QsId_object_map[question.question_id] = question;
+            last_id = max(last_id,question.question_id);
+            if(question.parent_question_id != -1) // in case of child qs, we need to add child to map
+                parentQsId_childQsId_map[question.parent_question_id].push_back(question.question_id);
+            else
+                parentQsId_childQsId_map[question.question_id].push_back(question.question_id);
+        }
+        
+    }
+    void Update_Questions()
+    {
+        string path = "data/questions.txt";
+        vector<string> lines;
+        for(auto& pair : QsId_object_map)
+            lines.push_back(pair.second.To_Line());
+
+        WriteFileLines(path,lines);
+    }
+    void Answer_Question(User& user)
+    {
+        Load_Quesitons();
+        int question_id = Read_Question_Id(user);
+        Question& question = QsId_object_map[question_id];
+        question.Print_Question_To();
+        if(question.answer != "")
+            cout<<"\nWarning: Already answerd. Answer will be updated\n";
+        cout<<"Enter Answer: ";
+        cin.ignore();
+        getline(cin,question.answer);
+
+    }
+    
+    int Read_Question_Id(User &user)
+    {
+        cout<<"Enetr Question id or -1 to cancel";
+        int qs_id;
+        cin>>qs_id;
+        if(qs_id == -1)
+            return -1;
+        if(!QsId_object_map.count(qs_id)){
+            cout << "No thread question with such ID. Try againز\n";
+            return Read_Question_Id(user);}
+        
+        Question &question = QsId_object_map[qs_id];
+        if(question.to_user_id != user.id){
+            cout << "This question is not for you.\n";
+            return Read_Question_Id(user);}
+        return qs_id;
+
+    }
+
     int read_parentQs_id()
     {
         cout<<"For thread question enter question id or -1 for new questions: ";
@@ -155,12 +358,21 @@ struct Question_Controller
 
 };
 
-
 struct AskMe_System
 {
     User_Controller user_controller;
     Question_Controller question_controller;
 
+
+    void Answer_Question()
+    {
+        cout<<"Enetr Question id or -1 to cancel";
+        int qs_id;
+        cin>>qs_id;
+        if(qs_id == -1)
+            return;
+        question_controller.Answer_Question(user_controller.current_user);
+    }
     void Ask_Question()
     {
         pair to_user_id = user_controller.ReadUserId();
@@ -168,7 +380,12 @@ struct AskMe_System
             return;
         question_controller.Ask_Question(user_controller.current_user,to_user_id);
     }
-    
+
+    void Load_Database()
+    {
+        question_controller.Load_Quesitons();
+
+    }
     void AccessSystem()
     {
         int choice = first_menu();
@@ -206,8 +423,11 @@ struct AskMe_System
         while (true)
         {
             int choice = second_menu();
-            if(choice == 5)
+            if(choice == 3)
+                Answer_Question();
+            else if(choice == 5)
                 Ask_Question();
+            
         }
     }
 
